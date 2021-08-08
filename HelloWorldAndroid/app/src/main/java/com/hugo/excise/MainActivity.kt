d9e1fcc9b52bd98a934cdfe6e6c998bd7912f8e2
Hugo <lipeng@qiniu.com>
2021-08-08 16:10:27 +0800

    update

--- HelloWorldAndroid/app/src/main/java/com/hugo/excise/MainActivity.kt
+++ HelloWorldAndroid/app/src/main/java/com/hugo/excise/MainActivity.kt
@@ -5,10 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var test = MainActivity()
+        test.init()
+        vars(1, 2, 3, 4, 5)
+    }
 
+    private fun vars(vararg v: Int) {
+        for (vt in v) {
+            print(vt)
+        }
+    }
+
+    private fun init() {
+        name = "hugo"
     }
 }
