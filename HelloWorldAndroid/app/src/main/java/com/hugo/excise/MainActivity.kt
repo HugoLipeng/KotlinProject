f2b348f90c82c0798748e52ec24d6ca0436377df
HugoLipeng <lipeng@qiniu.com>
2022-07-02 17:32:14 +0800

    Lambda简化过程

--- HelloWorldAndroid/app/src/main/java/com/hugo/excise/MainActivity.kt
+++ HelloWorldAndroid/app/src/main/java/com/hugo/excise/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hugo.excise
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     lateinit var name: String
@@ -10,9 +11,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val image = findViewById<View>(R.id.imageView)
         var test = MainActivity()
         test.init()
         vars(1, 2, 3, 4, 5)
+
+        // 1 原始代码，它的本质是用 object 关键字定义了一个匿名内部类
+        image.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                gotoPreview(v)
+            }
+        })
+        // 2  object 关键字可以被省略，这时候它在语法层面就不再是匿名内部类了，它更像是 Lambda 表达式了，因此它里面 override 的方法也要跟着删掉
+        image.setOnClickListener(View.OnClickListener{v: View? -> gotoPreview(v)})
+        // 3 View.OnClickListener被称为 SAM Constructor（SAM 构造器），它是编译器为我们生成的。由于 Kotlin 的 Lambda 表达式是不需要 SAM Constructor 的，所以它也可以被删掉
+        image.setOnClickListener({v: View? -> gotoPreview(v)})
+        // 4 由于 Kotlin 支持类型推导，所以 View 可以被删掉：
+        image.setOnClickListener({v -> gotoPreview(v)})
+        // 5 当 Kotlin Lambda 表达式只有一个参数的时候，它可以被写成 it：
+        image.setOnClickListener({it -> gotoPreview(it)})
+        // 6 Kotlin Lambda 的 it 是可以被省略的：
+        image.setOnClickListener({gotoPreview(it)})
+        // 7 当 Kotlin Lambda 作为函数的最后一个参数时，Lambda 可以被挪到外面：
+        image.setOnClickListener(){gotoPreview(it)}
+        // 8 当 Kotlin 只有一个 Lambda 作为函数参数时，() 可以被省略：
+        image.setOnClickListener{gotoPreview(it)}
+    }
+
+    private fun gotoPreview(v: View?) {
+
     }
 
     private fun vars(vararg v: Int) {
