c2b5e7fa96fcd4088479b9a34e3e47ea9be6dc80
HugoLipeng <lipeng@qiniu.com>
2022-11-13 15:15:34 +0800

    iterate

---
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/KotlinProgrammingPractice/iterate.kt
@@ -0,0 +1,11 @@
+package com.hugo.excise.KotlinProgrammingPractice
+
+fun main() {
+    val names = arrayOf("Tom", "Jerry", "Spike")
+    for (index in names.indices) {
+        println("Position of ${names.get(index)} is $index")
+    }
+    for ((index, name) in names.withIndex()) {
+        println("Position of $name is $index")
+    }
+}
