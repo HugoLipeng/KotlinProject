31980f6443e3a341224279bc00ab8f53c5dbbde8
HugoLipeng <lipeng@qiniu.com>
2022-11-13 08:30:13 +0800

    vararg可变数量的实参

---
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/KotlinProgrammingPractice/vararg.kt
@@ -0,0 +1,21 @@
+package com.hugo.excise.KotlinProgrammingPractice
+
+
+fun max(vararg numbers: Int): Int {
+    var large = Int.MIN_VALUE
+    for (number in numbers) {
+        large = if (number > large) number else large
+    }
+    return large
+}
+
+fun greetMany(msg: String, vararg names: String) {
+    println("$msg ${names.joinToString(", ")}")
+}
+
+fun main() {
+    println(max(1, 5, 2)) // 5
+    println(max(1, 5, 2, 12, 7, 3)) // 12
+    greetMany(msg = "Hello", "Tom", "Jerry", "Spike")
+}
+
