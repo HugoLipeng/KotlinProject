9bfe0e5290c3f89a8e681af62bf97dc6228b7101
HugoLipeng <lipeng@qiniu.com>
2022-11-13 15:26:34 +0800

    when的变量和作用域

--- KotlinBasic/src/main/kotlin/com/hugo/excise/KotlinProgrammingPractice/availableProcessors.kt
+++
@@ -1,5 +0,0 @@
-package com.hugo.excise.KotlinProgrammingPractice
-
-fun main(){
-    println(Runtime.getRuntime().availableProcessors())
-}
---
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/KotlinProgrammingPractice/cores.kt
@@ -0,0 +1,13 @@
+package com.hugo.excise.KotlinProgrammingPractice
+
+fun main() {
+    println(systemInfo())
+}
+
+fun systemInfo(): String {
+    return when (val numberofCores = Runtime.getRuntime().availableProcessors()) {
+        1 -> "1 core,packing this one to the museum"
+        in 2..16 -> "You have $numberofCores cores"
+        else -> "SnumberofCores cores!,I want your machine"
+    }
+}
