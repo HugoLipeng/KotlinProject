0a3b5dd9131781afe6862c0605d78dc2beb068d6
HugoLipeng <lipeng@qiniu.com>
2022-11-13 08:48:13 +0800

    spread运算符

---
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/KotlinProgrammingPractice/spread.kt
@@ -0,0 +1,9 @@
+package com.hugo.excise.KotlinProgrammingPractice
+
+
+fun main() {
+    val values = intArrayOf(1, 21, 3)
+    println(max(values[0], values[1], values[2])) // don‘t
+    println(max(*values)) // * ==> spread
+    println(max(*listOf(1, 4, 18, 12).toIntArray()))
+}
