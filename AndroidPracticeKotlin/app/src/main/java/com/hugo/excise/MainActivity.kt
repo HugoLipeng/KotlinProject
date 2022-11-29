709b9d7150b502c7324b6e72c31623d7d9c80644
HugoLipeng <lipeng@qiniu.com>
2022-11-29 22:59:37 +0800

    RecyclerView

--- AndroidPracticeKotlin/app/src/main/java/com/hugo/excise/MainActivity.kt
+++ AndroidPracticeKotlin/app/src/main/java/com/hugo/excise/MainActivity.kt
@@ -6,9 +6,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.hugo.excise.view.Fruit
+import com.hugo.excise.view.FruitAdapter
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     lateinit var name: String
+    private val fruitList = ArrayList<Fruit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         var test = MainActivity()
         test.init()
         vars(1, 2, 3, 4, 5)
+        initFruits() // 初始化水果数据
+        val layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
+        val adapter = FruitAdapter(fruitList)
+        recyclerView.adapter = adapter
 
 
         // 1 原始代码，它的本质是用 object 关键字定义了一个匿名内部类
@@ -58,6 +69,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         name = "hugo"
+    }
+
+    private fun initFruits() {
+        repeat(2) {
+            fruitList.add(Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic))
+            fruitList.add(Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic))
+            fruitList.add(Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic))
+            fruitList.add(Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic))
+            fruitList.add(Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic))
+            fruitList.add(Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic))
+            fruitList.add(Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic))
+            fruitList.add(Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic))
+            fruitList.add(Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic))
+            fruitList.add(Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic))
+        }
+    }
+
+    private fun getRandomLengthName(name: String): String {
+        val length = Random().nextInt(20) + 1
+        val builder = StringBuilder()
+        for (i in 0 until length) {
+            builder.append(name)
+        }
+        return builder.toString()
     }
 }
 
