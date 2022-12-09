d88372423945ee4e16292a27b8c7d5bff6e5ed31
HugoLipeng <lipeng@qiniu.com>
2022-12-09 23:19:35 +0800

    扩展函数 FilesKt

--- KotlinBasic/src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/Test.kt
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/Test.kt
@@ -1,6 +1,11 @@
 package com.hugo.excise.jktime.kotlinprimer
 
+import java.io.File
+
 
 fun main() {
     TestStatic.sayMessage("hello")
+
+    val file = File("src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/test.txt")
+    println(file.readText())
 }
--- KotlinBasic/src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/TestJava.java
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/TestJava.java
@@ -1,8 +1,17 @@
 package com.hugo.excise.jktime.kotlinprimer;
 
+import kotlin.io.FilesKt;
+import kotlin.text.Charsets;
+
+import java.io.File;
+
 public class TestJava {
     public static void main(String[] args) {
 //        TestStatic.INSTANCE.sayMessage("hello");
         TestStatic.sayMessage("hello");
+
+        File file = new File("src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/test.txt");
+        String content = FilesKt.readText(file, Charsets.UTF_8);
+        System.out.println(content);
     }
 }
