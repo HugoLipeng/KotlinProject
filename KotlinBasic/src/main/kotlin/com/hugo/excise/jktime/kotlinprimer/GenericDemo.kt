6798b4229effb68eb613e73e8b58d6839ac47cbf
HugoLipeng <lipeng@qiniu.com>
2022-12-25 15:14:41 +0800

    reified

--- KotlinBasic/src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/GenericDemo.kt
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/jktime/kotlinprimer/GenericDemo.kt
@@ -1,5 +1,6 @@
 package com.hugo.excise.jktime.kotlinprimer
 
+import com.google.gson.Gson
 /**
  * @author hugo
  * @date 2022-12-25 15:05
@@ -29,7 +30,30 @@ interface Callback {
     fun callback()
 }
 
+inline fun <reified T> Gson.fromJson(json: String): T {
+    return fromJson(json, T::class.java)
+}
+
+class View<T>(val clazz: Class<T>) {
+    val presenter by lazy { clazz.newInstance() }
+
+    companion object {
+        inline operator fun <reified T> invoke() = View(T::class.java)
+    }
+}
+
+class Presenter {
+    override fun toString(): String {
+        return "presenter"
+    }
+}
+
 fun main(args: Array<String>) {
     val test = Test<B>()
     test.add(B())
+
+    val b = View<Presenter>().presenter
+    val a = View.Companion.invoke<Presenter>().presenter
+    println(a)
+    println(b)
 }
