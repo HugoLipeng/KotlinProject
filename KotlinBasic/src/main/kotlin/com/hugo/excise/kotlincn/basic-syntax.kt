53e6c64982237f813a70703b425e942da87f6a35
HugoLipeng <lipeng@qiniu.com>
2023-07-05 21:13:48 +0800

    对一个对象实例调用多个方法 （with）

--- KotlinBasic/src/main/kotlin/com/hugo/excise/kotlincn/basic-syntax.kt
+++ KotlinBasic/src/main/kotlin/com/hugo/excise/kotlincn/basic-syntax.kt
@@ -19,6 +19,32 @@ fun main() {
     println(sum(1, 2))
 
     println(describe(1))
+
+    // 使用 in 操作符来检测某个数字是否在指定区间内。
+    val x = 10
+    val y = 9
+    if (x in 1..y + 1) {
+        println("fits in range")
+    }
+
+    // 使用 lambda 表达式来过滤（filter）与映射（map）集合
+    val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+    fruits
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.uppercase() }
+        .forEach { println(it) }
+
+    // 对一个对象实例调用多个方法 （with）
+    val myTurtle = Turtle()
+    with(myTurtle) { // 画一个 100 像素的正方形
+        penDown()
+        for (i in 1..4) {
+            forward(100.0)
+            turn(90.0)
+        }
+        penUp()
+    }
 }
 
 fun sum(a: Int, b: Int): Int {
@@ -33,3 +59,18 @@ fun describe(obj: Any): String =
         !is String -> "Not a string"
         else -> "Unknown"
     }
+
+class Turtle {
+    fun penDown(){
+
+    }
+    fun penUp(){
+
+    }
+    fun turn(degrees: Double){
+
+    }
+    fun forward(pixels: Double){
+
+    }
+}
